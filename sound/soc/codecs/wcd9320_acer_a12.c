ef1138121dd6878fd842e894fb47c3b807f67160
Vachounet <vachounet@live.fr>
2014-07-08 16:58:22 +0200

    Sound Control: (Optional) work around for WCD93xx audio issues
    (Use this only for devices with audio reset issues)
    
    Also bump version to 3.1
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    wcd9xxx-core: add register write without mutex protection
    
    This is assuming the calling function will take care of the mutex.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    
    	include/linux/mfd/wcd9xxx/core.h
    	sound/soc/codecs/wcd9320.c
    
    Signed-off-by: DooMLoRD <metoo.mememe@gmail.com>
    
    Patched for Z1 4.3 update (reverted from commit e1542764e7d7396992296a5df5f1d42ffba79742)

--- sound/soc/codecs/wcd9320_acer_a12.c
+++ sound/soc/codecs/wcd9320_acer_a12.c
@@ -4255,22 +4255,10 @@ static int taiko_volatile(struct snd_soc_codec *ssc, unsigned int reg)
 	return 0;
 }
 
-#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
-extern int snd_hax_reg_access(unsigned int);
-extern unsigned int snd_hax_cache_read(unsigned int);
-extern void snd_hax_cache_write(unsigned int, unsigned int);
-#endif
-
-#ifndef CONFIG_SOUND_CONTROL_HAX_3_GPL
-static
-#endif
-int taiko_write(struct snd_soc_codec *codec, unsigned int reg,
+static int taiko_write(struct snd_soc_codec *codec, unsigned int reg,
 	unsigned int value)
 {
 	int ret;
-#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
-	int val;
-#endif
 
 	if (reg == SND_SOC_NOPM)
 		return 0;
@@ -4284,28 +4272,9 @@ int taiko_write(struct snd_soc_codec *codec, unsigned int reg,
 				reg, ret);
 	}
 
-#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
-	if (!snd_hax_reg_access(reg)) {
-		if (!((val = snd_hax_cache_read(reg)) != -1)) {
-			val = wcd9xxx_reg_read_safe(&wcd9xxx->core_res, reg);
-		}
-	} else {
-		snd_hax_cache_write(reg, value);
-		val = value;
-	}
-	return wcd9xxx_reg_write(&wcd9xxx->core_res, reg, val);
-#else
-	return wcd9xxx_reg_write(&wcd9xxx->core_res, reg, value);
-#endifurn wcd9xxx_reg_write(&wcd9xxx->core_res, reg, value);
-#endif
+	return wcd9xxx_reg_write(codec->control_data, reg, value);
 }
-#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
-EXPORT_SYMBOL(taiko_write);
-#endif
-#ifndef CONFIG_SOUND_CONTROL_HAX_3_GPL 
-static
-#endif
-unsigned int taiko_read(struct snd_soc_codec *codec,
+static unsigned int taiko_read(struct snd_soc_codec *codec,
 				unsigned int reg)
 {
 	unsigned int val;
@@ -4326,12 +4295,9 @@ unsigned int taiko_read(struct snd_soc_codec *codec,
 				reg, ret);
 	}
 
-	val = wcd9xxx_reg_read(&wcd9xxx->core_res, reg);
+	val = wcd9xxx_reg_read(codec->control_data, reg);
 	return val;
 }
-#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
-EXPORT_SYMBOL(taiko_read);
-#endif
 
 static int taiko_startup(struct snd_pcm_substream *substream,
 		struct snd_soc_dai *dai)
