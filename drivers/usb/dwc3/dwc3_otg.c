9046e1d5b942b10fee756c58cd0659731943fe34
Vachounet <vachounet@live.fr>
2014-07-10 11:08:53 +0200

    fastcharge: initial Nexus 5 adaptation
    added custom charge levels for better control over fastcharge
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Signed-off-by: DooMLoRD <metoo.mememe@gmail.com>

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -23,6 +23,10 @@
 #include "dwc3_otg.h"
 #include "io.h"
 #include "xhci.h"
+ 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
 
 #ifdef CONFIG_MACH_ACER_A12
 #include "../../../arch/arm/mach-msm/acer_hw_version.h"
@@ -785,6 +789,17 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 					work = 1;
 					break;
 				case DWC3_SDP_CHARGER:
+#ifdef CONFIG_FORCE_FAST_CHARGE
+					if (force_fast_charge > 0)
+						dwc3_otg_set_power(phy,
+							DWC3_IDEV_CHG_MAX);
+					else
+						dwc3_otg_set_power(phy,
+							DWC3_IDEV_CHG_MIN);
+#else
+ 					dwc3_otg_set_power(phy,
+ 							DWC3_IDEV_CHG_MIN);
+#endif
 					dwc3_otg_start_peripheral(&dotg->otg,
 									1);
 					phy->state = OTG_STATE_B_PERIPHERAL;
